8622fc3cfce0c1e5da26d4cb3662ebe267267286
Emmanuel Lécharny <elecharny@apache.org>
2012-07-31 16:25:28 +0000

    o Added the serializer packahe o Added the ElementSerializer interface o Added the IntSerializer o Added a test for this class

---
+++ mavibot/src/main/java/org/apache/mavibot/btree/serializer/ElementSerializer.java
@@ -0,0 +1,48 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.mavibot.btree.serializer;
+
+
+/**
+ * This interface is used by implementations elements serializers.
+ * 
+ * @param <T> The type for the element
+ * 
+ * @author <a href="mailto:labs@labs.apache.org">Mavibot labs Project</a>
+ */
+public interface ElementSerializer<T>
+{
+    /**
+     * Produce the byte[] representation of the element
+     * 
+     * @param key The element to serialize
+     * @return The byte[] containing the serialized element
+     */
+    byte[] serialize( T key );
+
+
+    /**
+     * Deserialize an element from a byte[]
+     * 
+     * @param in The incoming byte[]
+     * @return The deserialized element
+     */
+    T deserialize( byte[] in );
+}
---
+++ mavibot/src/main/java/org/apache/mavibot/btree/serializer/Serializer.java
@@ -0,0 +1,67 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.mavibot.btree.serializer;
+
+
+/**
+ * This interface is used by implementations of the key and value serializers.
+ * 
+ * @param <K> The type for the keys
+ * @param <V> The type for the stored values
+ * 
+ * @author <a href="mailto:labs@labs.apache.org">Mavibot labs Project</a>
+ */
+public interface Serializer<K, V>
+{
+    /**
+     * Produce the byte[] representation of the key
+     * 
+     * @param key The key to serialize
+     * @return The byte[] containing the serialized key
+     */
+    byte[] serializeKey( K key );
+
+
+    /**
+     * Deserialize a key from a byte[]
+     * 
+     * @param in The incoming byte[]
+     * @return The deserialized key
+     */
+    K deserializeKey( byte[] in );
+
+
+    /**
+     * Produce the byte[] representation of the value
+     * 
+     * @param value The value to serialize
+     * @return The byte[] containing the serialized value
+     */
+    byte[] serializeValue( V value );
+
+
+    /**
+     * Deserialize a value from a byte[]
+     * 
+     * @param in The incoming byte[]
+     * @return The deserialized value
+     */
+    V deserializeValue( byte[] in );
+}
