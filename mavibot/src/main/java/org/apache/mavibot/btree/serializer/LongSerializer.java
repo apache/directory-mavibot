729a9c918f5ef0cde1078f32a55cbc26906f85d8
Emmanuel Lécharny <elecharny@apache.org>
2012-07-31 17:08:11 +0000

    Added the LongSerializer class and its associated test

---
+++ mavibot/src/main/java/org/apache/mavibot/btree/serializer/LongSerializer.java
@@ -0,0 +1,67 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.mavibot.btree.serializer;
+
+
+/**
+ * The Long serializer.
+ * 
+ * @author <a href="mailto:labs@labs.apache.org">Mavibot labs Project</a>
+ */
+public class LongSerializer implements ElementSerializer<Long>
+{
+    /**
+     * {@inheritDoc}
+     */
+    public byte[] serialize( Long element )
+    {
+        byte[] bytes = new byte[8];
+        long value = element.longValue();
+
+        bytes[0] = ( byte ) ( value >>> 56 );
+        bytes[1] = ( byte ) ( value >>> 48 );
+        bytes[2] = ( byte ) ( value >>> 40 );
+        bytes[3] = ( byte ) ( value >>> 32 );
+        bytes[4] = ( byte ) ( value >>> 24 );
+        bytes[5] = ( byte ) ( value >>> 16 );
+        bytes[6] = ( byte ) ( value >>> 8 );
+        bytes[7] = ( byte ) ( value );
+
+        return bytes;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public Long deserialize( byte[] in )
+    {
+        long result = ( ( long ) in[0] << 56 ) +
+            ( ( in[1] & 0xFFL ) << 48 ) +
+            ( ( in[2] & 0xFFL ) << 40 ) +
+            ( ( in[3] & 0xFFL ) << 32 ) +
+            ( ( in[4] & 0xFFL ) << 24 ) +
+            ( ( in[5] & 0xFFL ) << 16 ) +
+            ( ( in[6] & 0xFFL ) << 8 ) +
+            ( in[7] & 0xFFL );
+
+        return result;
+    }
+}
