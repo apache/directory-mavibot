7f77efe3db018048c6d86a8ce16109c1a0f9a65f
Kiran Ayyagari <kayyagari@apache.org>
2013-05-13 06:57:27 +0000

    hold the value container in a soft reference if the parent btree is in managed mode

--- mavibot/src/main/java/org/apache/mavibot/btree/DuplicateKeyMemoryHolder.java
+++ mavibot/src/main/java/org/apache/mavibot/btree/DuplicateKeyMemoryHolder.java
@@ -21,7 +21,10 @@ package org.apache.mavibot.btree;
 
 
 import java.io.IOException;
+import java.lang.ref.SoftReference;
 import java.util.UUID;
+
+import org.apache.mavibot.btree.exception.BTreeAlreadyManagedException;
 
 
 /**
@@ -37,9 +40,14 @@ public class DuplicateKeyMemoryHolder<K, V> implements ElementHolder<V, K, V>
     /** The BTree */
     private BTree<K, V> btree;
 
-    /** The reference to the Value instance, or null if it's not present */
+    /* the name of the value container btree. This value is set only when the parent BTree is in managed mode */
+    private String name;
+    
+    /* The reference to the Value instance, or null if it's not present. This will be null when the parent BTree is in managed mode */
     private BTree<V, V> valueContainer;
 
+    /* This value is set only when the parent BTree is in managed mode */
+    private SoftReference<BTree<V, V>> reference;
 
     /**
      * Create a new holder storing an offset and a SoftReference containing the value.
@@ -53,9 +61,31 @@ public class DuplicateKeyMemoryHolder<K, V> implements ElementHolder<V, K, V>
 
         try
         {
-            this.valueContainer = new BTree<V, V>( UUID.randomUUID().toString(), btree.getValueSerializer(),
+            BTree<V, V> valueContainer = new BTree<V, V>( UUID.randomUUID().toString(), btree.getValueSerializer(),
                 btree.getValueSerializer() );
-            valueContainer.init();
+            
+            
+            if( btree.isManaged() )
+            {
+                this.name = valueContainer.getName();
+                
+                try
+                {
+                    btree.getRecordManager().manage( valueContainer );
+                }
+                catch( BTreeAlreadyManagedException e )
+                {
+                    // should never happen
+                    throw new RuntimeException( e );
+                }
+                
+                reference = new SoftReference<BTree<V,V>>( valueContainer );
+            }
+            else
+            {
+                this.valueContainer = valueContainer;
+            }
+            
             valueContainer.insert( value, null, 0 );
         }
         catch ( IOException e )
@@ -64,14 +94,43 @@ public class DuplicateKeyMemoryHolder<K, V> implements ElementHolder<V, K, V>
         }
     }
 
+    
+    /* No qualifier */ DuplicateKeyMemoryHolder( BTree<K, V> btree, BTree<V,V> valueContainer )
+    {
+        this.btree = btree;
+        
+        if( btree.isManaged() )
+        {
+            this.name = valueContainer.getName();
+            reference = new SoftReference<BTree<V,V>>( valueContainer );
+        }
+        else
+        {
+            this.valueContainer = valueContainer;
+        }
+    }
 
+    
     /**
      * {@inheritDoc}
      */
     @Override
     public V getValue( BTree<K, V> btree )
     {
+        if( !btree.isManaged() )
+        {
+            return ( V ) valueContainer;
+        }
+        
         // wrong cast to please compiler
+        BTree<V,V> valueContainer = reference.get();
+        
+        if( valueContainer == null )
+        {
+            valueContainer = btree.getRecordManager().getManagedTree( name );
+            reference = new SoftReference<BTree<V,V>>( valueContainer );
+        }
+        
         return ( V ) valueContainer;
     }
 
